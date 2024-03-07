d857d61850bd648b035621a5992370829a39b95e
Ove Sundal <osundal83@gmail.com>
2024-03-07 20:56:03 +0100

    add numerical data and setup basic conversation chain

--- app/chat/services/chat_services.py
+++ app/chat/services/chat_services.py
@@ -6,12 +6,12 @@ from langchain.prompts import ChatPromptTemplate
 # To control the randomness and creativity of the generated
 # text by an LLM, use temperature = 0.0
 chat = ChatOpenAI(model="gpt-4-0125-preview", temperature=0.0)
-memory = ConversationBufferMemory()
-conversation = ConversationChain(
-    llm=chat,
-    memory=memory,
-    verbose=True
-)
+
+# conversation = ConversationChain(
+#     llm=chat,
+#     memory=memory,
+#     verbose=True
+# )
 
 template_string = """Translate the text \
 that is delimited by triple backticks \
@@ -27,11 +27,21 @@ in a calm and respectful tone
 
 
 def ask_openai(prompt):
+    chat = ChatOpenAI(model="gpt-4-0125-preview", temperature=0.0)
+    memory = ConversationBufferMemory()
+
+    conversation = ConversationChain(
+        llm=chat,
+        memory=memory,
+        verbose=True
+    )
     user_message = prompt_template.format_messages(
         style=user_style,
         text=prompt)
+
 
     parsed_prompt = chat(user_message)
     chat_response = conversation.predict(input=parsed_prompt.content)
 
     return chat_response
+
