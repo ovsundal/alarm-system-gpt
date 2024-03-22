10d94f5b68caa421c0e0a0d4c87ad5b68ab9374c
Ove Sundal <osundal83@gmail.com>
2024-03-22 12:00:45 +0100

    find correct parameters based on user query

--- app/chat/services/services/chat_services.py
+++ app/chat/services/services/chat_services.py
@@ -6,7 +6,7 @@ from chat.services.agent.get_agent_executor import get_agent_executor
 def ask_openai(user_prompt):
 
     agent = get_agent_executor()
-    response = agent.invoke({"input": "Plot wpi and cpi relative to pressure for well_1"})
+    response = agent.invoke({"input": user_prompt})
     response['output'] = json.loads(response['output'])
 
     return response
@@ -16,11 +16,21 @@ def extract_data_from_llm_response(data_params):
     if data_params is None:
         return None
 
-    print(data_params)
+    well_name = data_params.get('well_name')
+    x_axis_dimension = data_params.get('x_axis_dimension')
+    y_axis_dimensions = data_params.get('y_axis_dimensions')
+
     with open('reservoir/data/static_reservoir_data.json', 'r') as f:
         static_reservoir_data = json.load(f)
 
-    filtered_data = [item for item in static_reservoir_data
-                     if item['well_name'] == data_params['well_name']]
+    # find correct well data
+    well_data = [item for item in static_reservoir_data
+                     if item['well_name'] == well_name]
+
+    # filter based on llm response parameters
+    required_attributes = [well_name, x_axis_dimension] + y_axis_dimensions
+
+    filtered_data = [{k: item[k] for k in required_attributes if k in item}
+                     for item in well_data]
 
     return filtered_data
