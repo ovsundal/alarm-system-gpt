aa05f9cce52acf066059914a264bcb0e0d6b5a3d
osund <osundal83@gmail.com>
2024-06-16 22:39:51 +0200

    add load_dotenv call in settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,11 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_path = os.path.join(os.path.dirname(__file__), '..\..', '.env')
+load_dotenv(dotenv_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
